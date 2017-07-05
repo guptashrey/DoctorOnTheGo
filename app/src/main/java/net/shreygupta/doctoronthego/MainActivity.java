0661bbae42068bf88af357744d5f6db974974686
Shrey Gupta <shrey.gupta1904@gmail.com>
2017-07-06 01:47:07 +0530

    Added Sign Up & Sign In

--- app/src/main/java/net/shreygupta/doctoronthego/MainActivity.java
+++ app/src/main/java/net/shreygupta/doctoronthego/MainActivity.java
@@ -1,5 +1,9 @@
 package net.shreygupta.doctoronthego;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout t = (TabLayout) findViewById(R.id.tab);
+
+        TabLayout.Tab signup = t.newTab();
+        signup.setText("SIGN UP");
+        t.addTab(signup);
+
+        TabLayout.Tab signin = t.newTab();
+        signin.setText("SIGN IN");
+        t.addTab(signin);
+
+        loadFragment(new SignUpFragment(), "SignUp");
+
+        t.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == 0){
+                    loadFragment(new SignUpFragment(), "SignUp");
+                }
+
+                if (tab.getPosition() == 1) {
+                    loadFragment(new SignInFragment(), "SignIn");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment, String tag){
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame, fragment, tag);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
     }
 }
