2a76fe9e4b6ced54a107ba75a351c1b7da902e0a
architkaila <archit.kaila@gmail.com>
2017-07-05 16:10:56 +0530

    Added Main Activity

--- app/src/main/java/net/shreygupta/doctoronthego/SplashActivity.java
+++ app/src/main/java/net/shreygupta/doctoronthego/SplashActivity.java
@@ -1,5 +1,6 @@
 package net.shreygupta.doctoronthego;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Thread(){
+            public void run(){
+                try{
+                    Thread.sleep(3000);
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
     }
 }
