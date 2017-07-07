3a9fa062b6e875cde0377f091a007fd5dbda86e0
Shrey Gupta <shrey.gupta1904@gmail.com>
2017-07-07 17:29:21 +0530

    Added Image Button

--- app/src/main/java/net/shreygupta/doctoronthego/ClientChooseActivity.java
+++ app/src/main/java/net/shreygupta/doctoronthego/ClientChooseActivity.java
@@ -3,8 +3,11 @@ package net.shreygupta.doctoronthego;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ClientChooseActivity extends AppCompatActivity {
 
@@ -13,9 +16,8 @@ public class ClientChooseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_choose);
 
-        Button patient_activity = (Button) findViewById(R.id.patient_activity);
-        Button doctor_activity = (Button) findViewById(R.id.doctor_activity);
-        Button admin_activity = (Button) findViewById(R.id.admin_activity);
+        ImageButton patient_activity = (ImageButton) findViewById(R.id.patient_activity);
+        ImageButton doctor_activity = (ImageButton) findViewById(R.id.doctor_activity);
 
         patient_activity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +32,22 @@ public class ClientChooseActivity extends AppCompatActivity {
                 startActivity(new Intent(ClientChooseActivity.this,DoctorActivity.class));
             }
         });
+    }
 
-        admin_activity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ClientChooseActivity.this,AdminActivity.class));
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater m = getMenuInflater();
+        m.inflate(R.menu.client_choose_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.admin_activity) {
+            startActivity(new Intent(ClientChooseActivity.this,AdminActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
