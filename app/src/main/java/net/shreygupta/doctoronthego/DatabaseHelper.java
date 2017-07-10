ac472834ae61ba71c594bf1a445ecf6916509ffd
Shrey Gupta <shrey.gupta1904@gmail.com>
2017-07-10 19:48:06 +0530

    Updated SQLite Functionality

--- app/src/main/java/net/shreygupta/doctoronthego/DatabaseHelper.java
+++ app/src/main/java/net/shreygupta/doctoronthego/DatabaseHelper.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by shreygupta on 10/07/17.
